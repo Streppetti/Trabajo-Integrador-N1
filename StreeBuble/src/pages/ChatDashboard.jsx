16b2eb93fd4532acc2b1deab46c633b3538ec685
Alan <streegonza@gmail.com>
2026-03-01 21:16:12 -0300

    Organización final

--- StreeBuble/src/pages/ChatDashboard.jsx
+++
@@ -1,9 +0,0 @@
-const ChatDashboard = () => {
-    return (
-        <div style={{ display: 'flex', height: '100vh' }}>
-            <h1>Pantalla de Chat</h1>
-        </div>
-    );
-};
-
-export default ChatDashboard;
